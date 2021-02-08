3ca46a0f4b2aa451d1f254891356240e983aa94f
kengoon <kengoon19@gmail.com>
2021-02-08 13:24:26 +0100

    added a callback to video method in DownloadSingle class

--- tubeVid/app.py
+++ tubeVid/app.py
@@ -1,14 +1,17 @@
 # -*- coding: utf-8 -*-
 
 # Indent width : 8 spaces
+import sys
 
 import pafy
 import os
+from hurry.filesize import size, alternative
 
-#Python Software Foundation
+# Python Software Foundation
 # Short demo on new PSF sponsorship app
-URL = "https://www.youtube.com/watch?v=0nD4UcGu_iQ"
+from pafy.backend_shared import get_status_string
 
+URL = "https://www.youtube.com/watch?v=NvKIL0Jtw5k"
 
 PATH = os.getcwd()
 '''
@@ -29,103 +32,109 @@ else:
 
 '''
 # DOC : https://pythonhosted.org/pafy/
-#YOUTUBE API :: https://developers.google.com/youtube/registering_an_application
+# YOUTUBE API :: https://developers.google.com/youtube/registering_an_application
 """
 Pafy warns of future removal of the default youtube API that comes with pafy.
 If you're using pafy when the default API is removed 
 Uncomment code below and enter API you obtained from youtube.
 """
 
-#pafy.set_api_key(key) # Uncomment to set API key
+
+# pafy.set_api_key(key) # Uncomment to set API key
 
 class DownloadSingle:
 
-        def __init__(self, url):
-                #pass
-                self.media = pafy.new(url) # New pafy object
+    def __init__(self, url):
+        # pass
+        self.media = pafy.new(url)  # New pafy object
 
-        def mediaTypes(self):
-                '''
-                All available streams (media types)
-                '''
-                v = self.media.allstreams
-                return v
+    def mediaTypes(self):
+        """
+        All available streams (media types)
+        """
+        return self.media.allstreams
 
-        def filesize(self, stream):
-                '''
-                stream : media (stream / pafy stream)
-                to get size.
-                e.g . 
-                '''
-                size = stream.get_filesize()
-                return size
+    @staticmethod
+    def filesize(stream):
+        """
+        stream : media (stream / pafy stream)
+        to get size.
+        e.g .
+        """
+        return stream.get_filesize()
 
-        def video(self, stream_url = False, savepath=None, preftype="any"):
-                ''' URL that could be used to watch video without download
-                 Using VLC or any other video player that suports that.
-                 '''
-                v = self.media
-                s = v.getbest(preftype = preftype)
-                print("Size is %s" % self.filesize(s))
+    def video(self, stream_url=False, savepath=None, preftype="any", callback=None):
+        """ URL that could be used to watch video without download
+        Using VLC or any other video player that suports that.
+        """
+        v = self.media
+        s = v.getbest(preftype=preftype)
 
-                filename = s.download(filepath = savepath, quiet=False) # quiet=True, to show download output
-                if stream_url: 
-                        return filename, s.url
+        s.download(filepath=savepath, quiet=True,
+                   callback=self.get)  # quiet=True, to show download output
+        if stream_url:
+            return self.media.title, s.url
 
-                return filename, None
+        return self.media.title, None
 
-        
-        def audio(self,  stream_url = False,
-                  savepath=None, preftype = "any"):
-                ''' URL that could be used to watch video without download
-                 Using VLC or any other video player that suports that.
-                 '''
-                aud = self.media.getbestaudio(preftype = preftype) # preftype = "m4a" or "ogg"
-                print("Size is %s" % self.filesize(aud))
-                filename = aud.download(filepath = savepath, quiet=False) # quiet=True, to show download output
-                if stream_url:
-                        return filename, aud.url
-                        
-                
-                return filename, None
+    @staticmethod
+    def get(total, a, b, c, d):
+        status_string = get_status_string("MB")
+        a = int(size(a).replace("M", ""))
+        status = status_string.format(a, b, c, d)
+        print(status, end="\r")
+
+    def audio(self, stream_url=False,
+              savepath=None, preftype="any"):
+        """ URL that could be used to watch video without download
+        Using VLC or any other video player that supports that.
+        """
+        aud = self.media.getbestaudio(preftype=preftype)  # preftype = "m4a" or "ogg"
+        print("Size is %s" % self.filesize(aud))
+        filename = aud.download(filepath=savepath, quiet=False)  # quiet=True, to show download output
+        if stream_url:
+            return filename, aud.url
+
+        return filename, None
+
 
 class DownloadPlaylist:
-        """pafy.get_playlist2() uses
+    """pafy.get_playlist2() uses
         version 3 of youtube’s api, making it able to
         retrieve playlists of over 200 items.
         """
 
-        def __init__(self, url):
-                self.playlist = pafy.get_playlist2(url)
+    def __init__(self, url):
+        self.playlist = pafy.get_playlist2(url)
 
+    def p_listaudio(self, num=5, stream_url=False,
+                    savepath=None, preftype="any"):
+        pass
 
-        def p_listaudio(self, num = 5, stream_url = False,
-                      savepath = None, preftype = "any"):
-                pass
-
-        def p_listvideo(self, num = 5, stream_url = False,
-                        savepath = None, preftype = "any"):
-                pass
+    def p_listvideo(self, num=5, stream_url=False,
+                    savepath=None, preftype="any"):
+        pass
 
 
 class Search:
-        """
+    """
         Search for videos and playlists on Youtube.
         Get video URL, then you could pass it to be downloaded.
         """
 
-        def videoName(self, name):
-                """
+    def videoName(self, name):
+        """
                 Search for video and return results
                 """
-                pass
+        pass
 
-        def playlist(self, name):
-                """
+    def playlist(self, name):
+        """
                 Search for Video playlist and return results
                 """
-                pass
+        pass
 
-vid = DownloadSingle(url = URL)
-filename,_ = vid.video(savepath=PATH)
+
+vid = DownloadSingle(url=URL)
+filename, _ = vid.video(savepath=PATH)
 print(filename, "   Downloaded ")
